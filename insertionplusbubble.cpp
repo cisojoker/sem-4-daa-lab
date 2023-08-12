1aa2614231cb462efa8d19cb2f20658b639cd975
INTRO_VERT <103921455+cisojoker@users.noreply.github.com>
2023-08-12 17:11:22 +0530

    Update insertionplusbubble.cpp

--- insertionplusbubble.cpp
+++ insertionplusbubble.cpp
@@ -11,6 +11,9 @@ void bubble(vector<int> arr){
             }
         }
     }
+    for(auto x:arr){
+        cout<<x;
+    }
     endg=clock();
     cout<<"time taken by bubble sort is "<<((endg-start)/CLOCKS_PER_SEC)<<"sec"<<endl;
 }
@@ -18,17 +21,20 @@ void insertion(vector<int> arr){
     int n=arr.size();
     double start,endg;
     start=clock();
-       for(int i=1;i<n;i++){
-           int j=i-1;
-          int temp=arr[i];
-           while(j>=0 && arr[i]<arr[j]){
-               arr[j+1]=arr[j];
-               j--;
-           }
-           arr[j+1]=temp;
+   for(int i=1;i<arr.size();i++){
+            int j=i-1;
+            int temp=arr[i];
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+   }
+       for(auto x:arr){
+           cout<<x;
        }
     endg=clock();
-    cout<<"time taken by bubble sort is "<<((endg-start)/CLOCKS_PER_SEC)<<"sec"<<endl;
+    cout<<"time taken by insertion sort is "<<((endg-start)/CLOCKS_PER_SEC)<<"sec"<<endl;
 }
 int main(){
     
@@ -38,11 +44,9 @@ int main(){
     vector<int>arr(n);
 
     for(int i=0;i<n;i++){
-        int a=rand()%(INT_MAX);
-        arr[i]=a;
-       
+       cout<<"enter the eleement";
+       cin>>arr[i];
     }
     bubble(arr);
-    
     insertion(arr);
 }
