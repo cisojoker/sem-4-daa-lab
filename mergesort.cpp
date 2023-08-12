e2a671fd00240f66c0a7476f5650461a3c413863
INTRO_VERT <103921455+cisojoker@users.noreply.github.com>
2023-08-12 17:43:09 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,4 +1,7 @@
-void merge(vector<int>&a,int si,int mid,int ei){
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int>&arr,int si,int mid,int ei){
     int n1=mid-si+1;
     int n2=ei-mid;
     int left[n1];
@@ -6,35 +9,39 @@ void merge(vector<int>&a,int si,int mid,int ei){
     int i=0;
     int j=0;
     int l=si;
-    for(int o=0;o<n1;o++){
-        left[o]=a[si+o];
+    for(int po=0;po<n1;po++){
+        left[po]=arr[po+si];
     }
-    for(int o=0;o<n2;o++){
-        right[o]=a[mid+1+o];
+     for(int po=0;po<n2;po++){
+        right[po]=arr[po+mid+1];
     }
-    while(n1>i && n2>j){
-        if (left[i]<=right[j]){
-            a[l]=left[i];i++;
+    while(i<n1 && j<n2){
+        if(left[i]<=right[j]){
+            arr[l]=left[i];
+            i++;
         }
-        else {
-            a[l]=right[j];
+        else{
+            arr[l]=right[j];
             j++;
-        }l++;
+        }
+        l++;
     }
-    while(n1>i){
-        a[l]=left[i];i++;l++;
+    while(i<n1){
+          arr[l]=left[i];
+            i++;l++;
     }
-    while(n2>j){
-         a[l]=right[j];
+    while(j<n2){
+         arr[l]=right[j];
             j++;l++;
     }
 }
-void mergesort(vector<int>&a,int si,int ei){
-    if (si>=ei)return ;
-   int mid=(si+ei)/2;
-    mergesort(a,si,mid);
-    mergesort(a,mid+1,ei);
-    merge(a,si,mid,ei);
+
+void mergesort(vector<int>& arr,int si ,int ei){
+    if(si>=ei)return ;
+    int mid =(si+ei)/2;
+    mergesort(arr,si,mid);
+    mergesort(arr,mid+1,ei);
+    merge(arr,si,mid,ei);
 }
 int main(){
     int n;
