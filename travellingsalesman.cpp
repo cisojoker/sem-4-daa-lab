e2d066108db21bac431390823ae9708d0dc0f79a
INTRO_VERT <103921455+cisojoker@users.noreply.github.com>
2023-08-13 00:38:18 +0530

    Update travellingsalesman.cpp

--- travellingsalesman.cpp
+++ travellingsalesman.cpp
@@ -1,32 +1,25 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
 int tsp(const vector<vector<int>>& cost) {
     int n = cost.size();
     vector<int> tour(n, 0);  // Initialize tour with city indices
-
     for (int i = 1; i < n; ++i) {
         tour[i] = i;
     }
-
     int min_cost = INT_MAX;
-
     do {
         int current_cost = 0;
         int current_city = 0;
-
         for (int i = 0; i < n; ++i) {
             current_cost += cost[current_city][tour[i]];
             current_city = tour[i];
         }
         current_cost += cost[current_city][0];  // Return to city 0
-
         min_cost = min(min_cost, current_cost);
-
     } while (next_permutation(tour.begin() + 1, tour.end()));
-
-    return min_cost;} 
+    return min_cost;
+} 
 int main()
 { 
     vector<vector<int>> cost = {
